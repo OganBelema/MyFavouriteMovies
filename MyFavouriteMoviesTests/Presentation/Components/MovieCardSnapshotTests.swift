497ba519d840726ee1d700847275bc8dd8b7dbd1
Oganbelema <oganbelema@gmail.com>
2026-05-11 17:15:59 +0100

    add screenshot test for MovieCard component

---
+++ MyFavouriteMoviesTests/Presentation/Components/MovieCardSnapshotTests.swift
@@ -0,0 +1,49 @@
+//
+//  MovieCardSnapshotTests.swift
+//  MyFavouriteMovies
+//
+//  Created by Belema on 11/05/2026.
+//
+
+import SnapshotTesting
+import SwiftUI
+import XCTest
+@testable import MyFavouriteMovies
+
+class MovieCardSnapshotTests: XCTestCase {
+    func testMovieCard() {
+        let movie = MovieUIData(
+            id: 1,
+            title: "Inception",
+            releaseDate: "2010-07-16",
+            overview: "Dom Cobb (Leonardo DiCaprio) is a thief with the rare ability to enter people's dreams and steal their secrets from their...",
+            posterPath: "",
+            popularity: 90.0,
+            voteAverage: 8.0,
+            isFavorite: true
+        )
+
+        let view = MovieCard(movie: movie, onFavouriteToggle: {}, onTap: {})
+        let vc = UIHostingController(rootView: view)
+        assertSnapshot(of: vc, as: .image(on: .iPhone13Pro), record: true)
+    }
+
+    func testMovieCard_DarkMode() {
+        let movie = MovieUIData(
+            id: 1,
+            title: "Inception",
+            releaseDate: "2010-07-16",
+            overview: "Dom Cobb (Leonardo DiCaprio) is a thief with the rare ability to enter people's dreams and steal their secrets from their...",
+            posterPath: "",
+            popularity: 90.0,
+            voteAverage: 8.0,
+            isFavorite: true
+        )
+
+        let view = MovieCard(movie: movie, onFavouriteToggle: {}, onTap: {})
+            .colorScheme(.dark)
+
+        let vc = UIHostingController(rootView: view)
+        assertSnapshot(of: vc, as: .image(on: .iPhone13Pro), record: true)
+    }
+}
