4c69eba670922d8a648a54ba7b4ee0b43c9accdb
Oganbelema <oganbelema@gmail.com>
2026-05-11 17:57:53 +0100

    add snapshot test for movie detail view

---
+++ MyFavouriteMoviesTests/Presentation/MovieDetail/MovieDetailSnapshotTests.swift
@@ -0,0 +1,50 @@
+//
+//  MovieDetailSnapshotTests.swift
+//  MyFavouriteMovies
+//
+//  Created by Belema on 11/05/2026.
+//
+
+import SnapshotTesting
+import SwiftUI
+import XCTest
+@testable import MyFavouriteMovies
+
+class MovieDetailSnapshotTests: XCTestCase {
+    func testMovieDetailView() {
+        let movie = MovieUIData(
+            id: 1,
+            title: "Inception",
+            releaseDate: "2010-07-16",
+            overview: "Dom Cobb (Leonardo DiCaprio) is a thief with the rare ability to enter people's dreams and steal their secrets from their...",
+            posterPath: "",
+            popularity: 90.0,
+            voteAverage: 8.0,
+            isFavorite: true
+        )
+        let view = MovieDetailView(movie: movie)
+        let vc = UIHostingController(rootView: view)
+        assertSnapshot(of: vc, as: .image(on: .iPhone13Pro), record: true)
+    }
+
+    func testMovieDetailView_DarkMode() {
+        let movie = MovieUIData(
+            id: 1,
+            title: "Inception",
+            releaseDate: "2010-07-16",
+            overview: "Dom Cobb (Leonardo DiCaprio) is a thief with the rare ability to enter people's dreams and steal their secrets from their...",
+            posterPath: "",
+            popularity: 90.0,
+            voteAverage: 8.0,
+            isFavorite: true
+        )
+        let view = MovieDetailView(movie: movie)
+            .preferredColorScheme(.dark)
+        let vc = UIHostingController(rootView: view)
+        assertSnapshot(
+            of: vc,
+            as: .image(on: .iPhone13Pro, traits: .init(userInterfaceStyle: .dark)),
+            record: true
+        )
+    }
+}
